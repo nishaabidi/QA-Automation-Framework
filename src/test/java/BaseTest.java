3b01f7ee524a7cf0e47091e6b8eece52e7af74a5
nishaabidi <nishaabidi@gmail.com>
2022-11-20 21:59:59 -0800

    Homework24 Complete

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,11 +5,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
@@ -37,9 +41,9 @@ public class BaseTest {
     // lets try adding it to our TestNG xml file..
     // now our launchbrowser method can now access baseurl parameter in TestNG xml file
     // lets try running it
-    public void launchBrowser(String baseURL) {
+    public void launchBrowser(String baseURL) throws MalformedURLException {
 
-//        driver = new ChromeDriver();
+        driver = new ChromeDriver();
         System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
         driver = new FirefoxDriver();
         driver = pickBrowser(System.getProperty("browser"));
@@ -50,13 +54,25 @@ public class BaseTest {
         url = baseURL;
         driver.get(url);
 
+
+
     }
 
-    private WebDriver pickBrowser(String browser) {
+    private WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities capabilities= new DesiredCapabilities();
+        String gridURL = "http://192.168.1.100:4444)";
         switch (browser){
             case "firefox":
                 System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
                 return driver = new FirefoxDriver();
+
+            case "grid-firefox":
+                capabilities.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+
+            case "grid-chrome":
+                capabilities.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
             default:
                 return driver = new ChromeDriver();
         }
